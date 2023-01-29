7f3f850ee6486dda2efaae8204661d09c4c394f8
kallyas <kallyasmedia@gmail.com>
2023-01-29 22:11:16 +0300

    ch: add layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="page-wrapper compact-wrapper" id="pageWrapper">
+      {/* header here */}
+      <div className="page-body-wrapper">
+        <div className="sidebar-wrapper">{/* sidebar here */}</div>
+        <div className="page-body">{children}</div>
+        <footer className="footer">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12 footer-copyright text-center">
+                <p className="mb-0">
+                  Copyright {new Date().getFullYear()} © Mindmate{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
