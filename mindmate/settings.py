c188640810c0658a1f1ecbf65c20b51bd9a653c7
Kally <60466044+kallyas@users.noreply.github.com>
2023-02-06 10:15:04 +0300

    Update settings.py

--- mindmate/settings.py
+++ mindmate/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mindmate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mindmate',
+        'USER': 'mindmate',
+        'PASSWORD': '1ghhxWdD1g7B09WqzvX92yguavnncSUn',
+        'HOST': 'dpg-cfga5a1a6gdma8n52sk0-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
